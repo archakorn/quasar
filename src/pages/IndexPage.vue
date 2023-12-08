e5eb5fe1507775886bc7e11102280b71b6b2cc21
archakorn <achakorn9909@gmail.com>
2023-12-08 11:14:51 +0700

    Archakorn 阿查科恩卡吉赫雷昆酒店  6304101380

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -19,7 +19,7 @@
        <q-input
         filled
         v-model="name"
-        label="Your name *"
+        label="您的姓氏 *"
         hint="Name and surname"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
